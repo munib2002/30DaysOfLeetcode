e2b49104db25c180772e6a22337ca962669228ec
Munib2002 <munib200002@gmail.com>
2023-10-10 22:24:06 +0500

    Day 11

---
+++ Day-11/976-largest-perimeter-triangle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const largestPerimeter = function (nums) {
+    nums.sort((a, b) => b - a);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] >= nums[i + 1] + nums[i + 2]) continue;
+
+        return nums[i] + nums[i + 1] + nums[i + 2];
+    }
+
+    return 0;
+};
